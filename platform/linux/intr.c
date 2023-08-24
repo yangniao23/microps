1a06a12556cf7b8aaba2ac8061416d35daba15ca
Yang Niao <yangniao23@gmail.com>
2023-08-24 19:40:58 +0900

    step5

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "net.h"
 #include "platform.h"
 #include "util.h"
 
@@ -72,6 +73,10 @@ static void *intr_thread(void *arg) {
             case SIGHUP:
                 terminate = 1;
                 break;
+            case SIGUSR1:
+                net_softirq_handler();
+                break;
+
             default:
                 for (entry = irqs; entry; entry = entry->next) {
                     if (entry->irq == (unsigned int)sig) {
@@ -115,5 +120,6 @@ int intr_init(void) {
     pthread_barrier_init(&barrier, NULL, 2);
     sigemptyset(&sigmask);
     sigaddset(&sigmask, SIGHUP);
+    sigaddset(&sigmask, SIGUSR1);
     return 0;
 }

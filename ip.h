114cac6305458b8dcdc685c6447748a5156e879a
Yang Niao <yangniao23@gmail.com>
2023-08-24 19:12:50 +0900

    step4

---
+++ ip.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern int ip_init(void);
--- net.c
+++ net.c
@@ -3,13 +3,29 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
+#include "ip.h"
 #include "platform.h"
 #include "util.h"
 
 /* NOTE: if you want to add/delete the entries after net_run(), you need to
  * protect these lists with a mutex. */
+static struct net_protocol *protocols;
 static struct net_device *devices;
+
+struct net_protocol {
+    struct net_protocol *next;
+    uint16_t type;
+    struct queue_head queue;
+    void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
+};
+
+struct net_protocol_queue_entry {
+    struct net_device *dev;
+    size_t len;
+    uint8_t data[];
+};
 
 struct net_device *net_device_alloc(void) {
     struct net_device *dev;
@@ -83,10 +99,53 @@ int net_device_output(struct net_device *dev, uint16_t type,
     return 0;
 }
 
+int net_protocol_register(uint16_t type,
+                          void (*handler)(const uint8_t *data, size_t len,
+                                          struct net_device *dev)) {
+    struct net_protocol *proto;
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            errorf("already registered, type=0x%04x", type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if (!proto) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("registred, type=0x%04x", type);
+    return 0;
+}
+
 int net_input_handler(uint16_t type, const uint8_t *data, size_t len,
                       struct net_device *dev) {
-    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
-    debugdump(data, len);
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (proto->type == type) {
+            entry = memory_alloc(sizeof(*entry) + len);
+            if (!entry) {
+                errorf("memory_alloc() failure");
+                return -1;
+            }
+            entry->dev = dev;
+            entry->len = len;
+            memcpy(entry->data, data, len);
+
+            queue_push(&proto->queue, entry);
+
+            debugf("queue pushed (num:%u), dev=%s, type=0x0%04x, len=%zu",
+                   proto->queue.num, dev->name, type, len);
+            return 0;
+        }
+    }
+    // unsupported protocol
     return 0;
 }
 
@@ -118,6 +177,10 @@ void net_shutdown(void) {
 int net_init(void) {
     if (intr_init() == -1) {
         errorf("intr_init() failure");
+        return -1;
+    }
+    if (ip_init() == -1) {
+        errorf("ip_init() failure");
         return -1;
     }
     infof("initialized");

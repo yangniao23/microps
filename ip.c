2705c16ff7ead1868e6466296349735b5fba5b12
Yang Niao <yangniao23@gmail.com>
2023-08-25 15:24:54 +0900

    step7

--- ip.c
+++ ip.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #include "net.h"
+#include "platform.h"
 #include "util.h"
 
 struct ip_hdr {
@@ -24,6 +25,10 @@ struct ip_hdr {
 
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+/* NOTE: if you want to add/delete the entries after net_run(), you need to
+ * protect these lists with a mutex. */
+static struct ip_iface *ifaces;
 
 int ip_addr_pton(const char *p, ip_addr_t *n) {
     char *sp, *ep;
@@ -85,10 +90,66 @@ static void ip_dump(const uint8_t *data, size_t len) {
 #endif
 }
 
+struct ip_iface *ip_iface_alloc(const char *unicast, const char *netmask) {
+    struct ip_iface *iface;
+
+    iface = memory_alloc(sizeof(*iface));
+    if (!iface) {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    NET_IFACE(iface)->family = NET_IFACE_FAMILY_IP;
+
+    if (ip_addr_pton(unicast, &iface->unicast)) {
+        errorf("convert unicast failure");
+        memory_free(iface);
+        return NULL;
+    }
+    if (ip_addr_pton(netmask, &iface->netmask)) {
+        errorf("convert netmask failure");
+        memory_free(iface);
+        return NULL;
+    }
+    iface->broadcast = (iface->unicast & iface->netmask) | ~(iface->netmask);
+
+    return iface;
+}
+
+/* NOTE: must not be call after net_run() */
+int ip_iface_register(struct net_device *dev, struct ip_iface *iface) {
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+    char addr3[IP_ADDR_STR_LEN];
+
+    if (net_device_add_iface(dev, (struct net_iface *)iface)) {
+        errorf("net_device_add_iface() failure");
+        return -1;
+    }
+
+    iface->next = ifaces;
+    ifaces = iface;
+
+    infof("registered: dev=%s, unicast=%s, netmask=%s, broadcast=%s", dev->name,
+          ip_addr_ntop(iface->unicast, addr1, sizeof(addr1)),
+          ip_addr_ntop(iface->netmask, addr2, sizeof(addr2)),
+          ip_addr_ntop(iface->broadcast, addr3, sizeof(addr3)));
+    return 0;
+}
+
+struct ip_iface *ip_iface_select(ip_addr_t addr) {
+    struct ip_iface *entry;
+    for (entry = ifaces; entry; entry = ifaces->next) {
+        if (entry->unicast == addr) return entry;
+    }
+    return NULL;
+}
+
 static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {
     struct ip_hdr *hdr;
     uint8_t v;
     uint16_t hlen, total, offset;
+    struct ip_iface *iface;
+    char addr[IP_ADDR_STR_LEN];
 
     if (len < IP_HDR_SIZE_MIN) {
         errorf("too short");
@@ -122,7 +183,17 @@ static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {
         errorf("fragments does not support");
         return;
     }
-    debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+
+    iface = (struct ip_iface *)net_device_get_iface(dev, NET_IFACE_FAMILY_IP);
+    if (!iface) return;
+
+    if (((hdr->dst) != iface->unicast) && ((hdr->dst) != iface->unicast) &&
+        ((hdr->dst) != IP_ADDR_BROADCAST))
+        return;
+
+    debugf("dev=%s, iface=%s, protocol=%u, total=%u", dev->name,
+           ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol,
+           total);
     ip_dump(data, total);
 }
 
--- net.c
+++ net.c
@@ -79,6 +79,32 @@ static int net_device_close(struct net_device *dev) {
     infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
     return 0;
 }
+int net_device_add_iface(struct net_device *dev, struct net_iface *iface) {
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == iface->family) {
+            errorf("already exists, dev=%s, family=%d", dev->name,
+                   entry->family);
+            return -1;
+        }
+    }
+    iface->next = dev->ifaces;
+    iface->dev = dev;
+    dev->ifaces = iface;
+
+    return 0;
+}
+
+struct net_iface *net_device_get_iface(struct net_device *dev, int family) {
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == family) return entry;
+    }
+
+    return NULL;
+}
 
 int net_device_output(struct net_device *dev, uint16_t type,
                       const uint8_t *data, size_t len, const void *dst) {

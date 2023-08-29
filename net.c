45c3b060b7f6caf77489125322ee9d9347f5355e
Yang Niao <yangniao23@gmail.com>
2023-08-29 18:30:53 +0900

    step22

--- net.c
+++ net.c
@@ -10,6 +10,7 @@
 #include "icmp.h"
 #include "ip.h"
 #include "platform.h"
+#include "tcp.h"
 #include "udp.h"
 #include "util.h"
 
@@ -315,6 +316,10 @@ int net_init(void) {
     }
     if (udp_init() == -1) {
         errorf("udp_init() failure");
+        return -1;
+    }
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
         return -1;
     }
     infof("initialized");
---
+++ tcp.h
@@ -0,0 +1,2 @@
+#pragma once
+extern int tcp_init(void);
